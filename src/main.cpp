40eaa6c11eab4489703564b1e80aa4cef8a96905
Rahul Pawar <pawarrahul.em@gmail.com>
2024-02-27 00:07:31 +0530

    Tokenize

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,97 @@
 #include<iostream>
 #include<fstream>
 #include<sstream>
+#include<vector>
 
+enum class TokenType {
+    sys_debug,
+    open_par,
+    close_par,
+    init_lit,
+    semi
+};
+
+struct Token {
+    TokenType type;
+    std::string value = "";
+};
+
+
+std::vector<Token> tokenize(const std::string& str) {
+    std::string buff;
+    std::vector<Token> tokens;
+    for(int i = 0; i < str.length(); i++) {
+        
+        char c = str.at(i);
+
+        if (std::isalpha(c)) {
+            buff.push_back(c);
+            i++;
+
+            while (std::isalnum(str.at(i))) {
+                buff.push_back(str.at(i));
+                i++;
+            }
+            i--;
+            
+            if (buff == "debug") {
+                tokens.push_back({
+                    .type = TokenType::sys_debug
+                });
+                buff.clear();
+                continue;
+
+            } else {
+                std::cerr << "Wrong Syntax " << std::endl;
+                exit (EXIT_FAILURE); 
+            }
+        }
+
+        else if (std::isspace(c)) {
+            continue;
+        }
+
+        else if (c == '(') {
+            tokens.push_back({
+                .type = TokenType::open_par
+            });
+            continue;
+        }
+
+        else if (c == ')') {
+            tokens.push_back({
+                .type = TokenType::close_par
+            });
+            continue;
+        }
+
+        else if (std::isdigit(c)) {
+            buff.push_back(c);
+            i++;
+
+            while(std::isdigit(str.at(i))){
+                buff.push_back(str.at(i));
+                i++;
+            }
+            i--;
+            tokens.push_back({
+                .type = TokenType::init_lit,
+                .value = buff
+            });
+            buff.clear();
+        }
+
+        else if (c == ';') {
+            tokens.push_back({
+                .type = TokenType::semi
+            });
+        } else {
+            std::cerr << "Wrong Syntax.." << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        return tokens;
+    }
+}
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cerr << "No file passed for execution.." << std::endl;
@@ -16,6 +106,8 @@ int main(int argc, char* argv[]) {
     }
 
     std::string contents = contents_stream.str();
+
+    tokenize(contents);
 
     std::cout << contents << std::endl;
     return EXIT_SUCCESS;
